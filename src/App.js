8bda44f008c058751860f479589ec40529f902ff
bdemers1055 <bdemers1055@gmail.com>
2018-07-18 19:51:31 -0400

    added a little styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import ComicBox from './ComicBox';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-                bloop
-                
+      <div className="AppContainer">
+      <ComicBox />
       </div>
     );
   }
---
+++ src/ComicBox.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './ComicBox.css';
+
+class ComicBox extends Component {
+    render(){
+        return(
+            <div className="ComicBoxContainer">comics</div>
+        )
+    };
+}
+
+export default ComicBox;
