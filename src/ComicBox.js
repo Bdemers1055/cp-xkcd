aacad05060baac2c87bb25562286a12ccc079203
bdemers1055 <bdemers1055@gmail.com>
2018-07-18 21:00:26 -0400

    success and error if else request json data, need to address cors issues before moving forward

--- src/ComicBox.js
+++ src/ComicBox.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './ComicBox.css';
 
 class ComicBox extends Component {
-    render(){
-        return(
-            <div className="ComicBoxContainer">comics</div>
-        )
+  constructor(props) {
+    super(props);
+    this.state = {
+        number: 0,
+        comic: {},
+        success: false,
+        error: null,
     };
+  }
+componentDidMount(){
+    const url = 'https://jsonplaceholder.typicode.com/users/1'
+    axios.get(url).then((response) => {
+        this.setState({
+            comic: response.data,
+            success: true,
+        });
+    }).catch((error) => {
+        this.setState({
+            success: false,
+            error,
+        });
+  });
+}
+render(){
+    const { success, error } = this.state;
+    if(error) {
+        return(
+            <p>oops,try again</p>
+        );
+    }
+    if (!success){
+        return(
+        <div>loading...</div>
+        );
+    }
+    return(
+        <div className="ComicBoxContainer">comics</div>
+    )
+};
 }
 
 export default ComicBox;
